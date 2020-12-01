423f9dbe1483f62db7dd1bcd049c132dd5aef783
andylamax <andylamax@programmer.net>
2020-12-01 21:08:44 +0300

    Initial commit

---
+++ paging-core/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.library")
+    kotlin("multiplatform")
+    id("tz.co.asoft.library")
+}
+
+
+kotlin {
+    universalLib()
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:${versions.kotlinx.coroutines}")
+                api("org.jetbrains.kotlinx:kotlinx-serialization-core:${versions.kotlinx.serialization}")
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-android:${versions.kotlinx.coroutines}")
+            }
+        }
+    }
+}
